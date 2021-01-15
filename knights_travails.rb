c2bf2f559ddf5585fdf40ea6f32d3e2c47b46c93
Kenny Li <kenny.wj.li@gmail.com>
2021-01-15 16:04:08 -0500

    added initialize

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,8 +1,11 @@
-require_relative "00_tree_node.rb"
+require_relative "./skeleton-2/lib/00_tree_node.rb"
 class KnightPathFinder
+    attr_reader :root_node
     def initialize(position)
         @board = Array.new(8){Array.new(8, " ")}
-        @position = positon
+        @root_node = PolyTreeNode.new(position)
     end
-
+    def build_move_tree
+        
+    end
 end
