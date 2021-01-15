4a1cdb7b1a7c72d78fc099a6c7d71e5f206be5e1
Kenny Li <kenny.wj.li@gmail.com>
2021-01-15 16:02:03 -0500

    added initialize

---
+++ knights_travails.rb
@@ -0,0 +1,8 @@
+require_relative "00_tree_node.rb"
+class KnightPathFinder
+    def initialize(position)
+        @board = Array.new(8){Array.new(8, " ")}
+        @position = positon
+    end
+
+end
