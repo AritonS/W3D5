fbb0ddaced38c53b46984c6206bf55cf46da4548
Ariton Sefedini <aritonsefedini@icloud.com>
2021-01-15 16:27:27 -0500

    Added valid_moves and new_move_positions

--- knights_travails.rb
+++ knights_travails.rb
@@ -5,7 +5,22 @@ class KnightPathFinder
         @board = Array.new(8){Array.new(8, " ")}
         @root_node = PolyTreeNode.new(position)
     end
+    # def [](pos)
+    #     row, col = pos 
+    #     @root_node[row][col].value
+    # end
+    def new_move_positions(pos)
+        if !@considered_positions.include?(pos)
+            KnightPathFinder.valid_moves(pos)
+        end
+    end
+    def self.valid_moves(pos) # [4, 3]
+        @considered_positions = [@root_node.value]
+    end
+    
+    
     def build_move_tree
-        
+        position = @root_node.value
+        @root_node.add_child(PolyTreeNode.new([position[0] + 2, position[1] + 1]))
     end
 end
